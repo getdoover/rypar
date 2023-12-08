fb8e10015de15713d9df4237a4155add5aec2ea7
jj-mac-vm <jarrod@span-eng.com>
2023-12-08 10:19:18 +1000

    added dial customization

--- processor/target.py
+++ processor/target.py
@@ -152,7 +152,7 @@ class target:
                         "type" : "uiVariable",
                         "varType" : "text",
                         "name" : "sensorLastRead",
-                        "displayString" : "Last Reading",
+                        "displayString" : "Reading taken at:",
                     },
                     "battVoltage" : {
                         "type" : "uiVariable",
@@ -232,6 +232,133 @@ class target:
                                         "showOnGraph" : True
                                     }
                                 ]
+                            },
+                            "sensor_settings_submodule": {
+                                "type": "uiSubmodule",
+                                "name": "sensor_settings_submodule",
+                                "displayString": "Temperature Dial Ranges",
+                                "children": {
+                                    "sensor_ranges": {
+                                        "type": "uiSubmodule",
+                                        "name": "sensor_ranges",
+                                        "displayString": "Temperature Dial Ranges",
+                                        "children": {
+                                            "maxLevel": {
+                                                "type": "uiFloatParam",
+                                                "name": "maxLevel",
+                                                "displayString": "Max Level (C)",
+                                            },
+                                            "maxMidLevel": {
+                                                "type": "uiFloatParam",
+                                                "name": "maxLevel",
+                                                "displayString": "Max-Mid Level (C)",
+                                            },
+                                            "midMinLevel": {
+                                                "type": "uiFloatParam",
+                                                "name": "midMinLevel",
+                                                "displayString": "Mid-Min Level (C)",
+                                            },
+                                            "midMinLevel": {
+                                                "type": "uiFloatParam",
+                                                "name": "midMinLevel",
+                                                "displayString": "Mid-Min Level (C)",
+                                            },
+                                            "minLevel": {
+                                                "type": "uiFloatParam",
+                                                "name": "minLevel",
+                                                "displayString": "Min Level (C)",
+                                            },
+                                        },
+                                        "sensor_range_colours": {
+                                            "type": "uiSubmodule",
+                                            "name": "sensor_ranges",
+                                            "displayString": "Sensor Range Colours",
+                                            "children": {
+                                                "minColourState":{
+                                                    "type": "uiStateCommand",
+                                                    "name": "minColourState",
+                                                    "displayString": "Min",
+                                                    "userOptions": {
+                                                        "green": {
+                                                            "type": "uiElement",
+                                                            "name": "green",
+                                                            "displayString": "Green"
+                                                        },
+                                                        "yellow": {
+                                                            "type": "uiElement",
+                                                            "name": "yellow",
+                                                            "displayString": "Yellow"
+                                                        },
+                                                        "red": {
+                                                            "type": "uiElement",
+                                                            "name": "red",
+                                                            "displayString": "Red"
+                                                        },
+                                                        "blue": {
+                                                            "type": "uiElement",
+                                                            "name": "blue",
+                                                            "displayString": "Blue"
+                                                        },
+                                                    }
+                                                },
+                                                "midColourState":{
+                                                    "type": "uiStateCommand",
+                                                    "name": "midColourState",
+                                                    "displayString": "Mid",
+                                                    "userOptions": {
+                                                        "green": {
+                                                            "type": "uiElement",
+                                                            "name": "green",
+                                                            "displayString": "Green"
+                                                        },
+                                                        "yellow": {
+                                                            "type": "uiElement",
+                                                            "name": "yellow",
+                                                            "displayString": "Yellow"
+                                                        },
+                                                        "red": {
+                                                            "type": "uiElement",
+                                                            "name": "red",
+                                                            "displayString": "Red"
+                                                        },
+                                                        "blue": {
+                                                            "type": "uiElement",
+                                                            "name": "blue",
+                                                            "displayString": "Blue"
+                                                        },
+                                                    }
+                                                },
+                                                "maxColourState":{
+                                                    "type": "uiStateCommand",
+                                                    "name": "maxColourState",
+                                                    "displayString": "Max",
+                                                    "userOptions": {
+                                                        "green": {
+                                                            "type": "uiElement",
+                                                            "name": "green",
+                                                            "displayString": "Green"
+                                                        },
+                                                        "yellow": {
+                                                            "type": "uiElement",
+                                                            "name": "yellow",
+                                                            "displayString": "Yellow"
+                                                        },
+                                                        "red": {
+                                                            "type": "uiElement",
+                                                            "name": "red",
+                                                            "displayString": "Red"
+                                                        },
+                                                        "blue": {
+                                                            "type": "uiElement",
+                                                            "name": "blue",
+                                                            "displayString": "Blue"
+                                                        },
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
                             },
                             "debug_submodule": {
                                 "type": "uiSubmodule",
@@ -379,6 +506,50 @@ class target:
 
     def uplink(self, oem_uplink_channel, ui_state_channel, ui_cmds_channel, location_channel):
         ## Run any uplink processing code here
+        cmds_obj = ui_cmds_channel.get_aggregate()
+
+        minColor = "red"
+        try: 
+            minColor = cmds_obj['cmds']['minColourState']
+        except Exception as e:
+            self.add_to_log("Error getting minColor - " + str(e))
+        
+        midColor = "yellow"
+        try:
+            midColor = cmds_obj['cmds']['midColourState']
+        except Exception as e:
+            self.add_to_log("Error getting midColor - " + str(e))
+
+        maxColor = "green"
+        try:
+            maxColor = cmds_obj['cmds']['maxColourState']
+        except Exception as e:
+            self.add_to_log("Error getting maxColor - " + str(e))
+
+        maxLevel = 100
+        try: 
+            maxLevel = cmds_obj['cmds']['maxLevel']
+        except Exception as e:
+            self.add_to_log("Error getting maxLevel - " + str(e))
+        
+        maxMidLevel = 70
+        try: 
+            maxMidLevel = cmds_obj['cmds']['maxMidLevel']
+        except Exception as e:
+            self.add_to_log("Error getting maxMidLevel - " + str(e))
+
+        midMinLevel = 30
+        try:
+            midMinLevel = cmds_obj['cmds']['midMinLevel']
+        except Exception as e:
+            self.add_to_log("Error getting midMinLevel - " + str(e))
+
+        minLevel = 0
+        try:
+            minLevel = cmds_obj['cmds']['minLevel']
+        except Exception as e:
+            self.add_to_log("Error getting minLevel - " + str(e))
+        
 
         if 'msg_obj' in self.kwargs and self.kwargs['msg_obj'] is not None:
             msg_id = self.kwargs['msg_obj']['message']
@@ -444,6 +615,29 @@ class target:
                         "sensorReading" : {
                             "displayString" : f"{sensor_name} ({reading_units})",
                             "currentValue" : reading,
+                            "ranges": [
+                                {
+                                    "label" : "Low",
+                                    "min" : minLevel,
+                                    "max" : midMinLevel,
+                                    "colour" : minColor,
+                                    "showOnGraph" : True
+                                },
+                                {
+                                    # "label" : "Ok",
+                                    "min" : midMinLevel,
+                                    "max" : maxMidLevel,
+                                    "colour" : midColor,
+                                    "showOnGraph" : True
+                                },
+                                {
+                                    "label" : "Fast",
+                                    "min" : maxMidLevel,
+                                    "max" : maxLevel,
+                                    "colour" : maxColor,
+                                    "showOnGraph" : True
+                                }
+                            ]
                         },
                         "sensorLastRead": {
                             "currentValue" : last_reading,
